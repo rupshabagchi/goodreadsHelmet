b7937eceb38acb3431eb2a1f25f5668951040419
rupshabagchi <rupsha.bagchi@gmail.com>
2022-02-04 10:24:59 +0200

    small fixes

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,23 +2,34 @@ import React, { useEffect, useState } from 'react';
 import List from './List';
 import './App.css';
 
-interface IState {
-  bestsellerList: [][]
+
+type BookProps = {
+  title: string,
+  description: string,
+  author: string,
+}
+
+type APIResponse = {
+  book_details: BookProps[]
 }
 
 
 const App = () => {
 
-  const [bestsellerList, setList] = useState<IState["bestsellerList"]>([]);
+  const [bestsellerList, setList] = useState<{}>([]);
 
   useEffect(() => {
     fetch(`https://api.nytimes.com/svc/books/v3/lists.json?list-name=hardcover-fiction&api-key= j4JErnSjmfdUNHlVSi1SIkwlnzwyd1Uv`, { method: 'get', })
       .then(response => response.json())
       .then(json => {
-        let bookData: [][] = [];
-        json.results.map((res: { book_details: [][] }) => {
-          bookData.push(res.book_details[0]);
-        })
+        let bookData: BookProps[] = [];
+        json.results.map((res: APIResponse) => bookData.push(res.book_details[0]));
+        bookData.map((book: BookProps) => ({
+          [book.title]: {
+            description: book.description,
+            author: book.author,
+          }
+        }));
         setList(bookData);
       })
   }, []);
--- app/src/List.tsx
+++ app/src/List.tsx
@@ -1,34 +1,26 @@
 import React from 'react';
 
-interface IProps {
-    book: {
-        author: string,
-        description: string,
-        title: string,
-        publisher: string,
-        age_group: string,
-        contributor?: string,
-        contributor_note?: string,
-        price?: string,
-        primary_isbn10: string,
-        primary_isbn13: string
-    },
-    bestsellerList: [][]
+type Book = {
+    title: string,
+    description: string,
+    author: string,
 }
 
-const List: React.FC<IProps> = ({ bestsellerList }) => {
+type BestsellerListProps = {[key: string]: {}};
+
+const List: React.FC<BestsellerListProps> = ({ bestsellerList }) => {
+    console.log(bestsellerList)
     const renderList = (): JSX.Element[] => {
-        return bestsellerList.map(book => {
+        return Object.values(bestsellerList).map((book: Book) => 
             return (<li className="List">
                 <div className="List-header">
-                    <h2>{book.title}</h2>
+                    <h2>{"hdhd"}</h2>
                 </div>
-                test
             </li>);
         })
-};
+    };
 
-return (<div>I am a List</div>);
+    return (<div>{renderList()}</div>);
 };
 
 export default List;
